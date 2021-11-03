39a553f11f6bcb430727e81553c2ba6cf3ce9f7b
Al2206 <77494086+Al2206@users.noreply.github.com>
2021-11-03 19:21:03 +0300

    Update Exercise2.java

--- src/HomeWork/Exercise2.java
+++ src/HomeWork/Exercise2.java
@@ -5,7 +5,7 @@ package HomeWork;
 public class Exercise2 {
     public static void main(String[] args) {
         for (int a = 1; a < 16; a ++ ) {
-            System.out.printf("Квадрат числа %d равен %d \n" , a,  a * a);// прикольно выводит значения!!!
+            System.out.println("Квадрат числа " +  a + " равен " + Math.pow(a, 2));// исправлено
         }
     }
 }
